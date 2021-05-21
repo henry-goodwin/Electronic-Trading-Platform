634e9131cc6805fc222c7482b1925606e4987ea6
Henry G <henrygdev@gmail.com>
2021-05-21 16:07:11 +1000

    Created Admin GUI

---
+++ src/com/company/GUI/AdminGUI/AdminFrame.java
@@ -0,0 +1,66 @@
+package com.company.GUI.AdminGUI;
+
+import com.company.Database.Users.UsersData;
+import com.company.GUI.AdminGUI.ManageUsersGUI.ManageUsersFrame;
+import com.company.GUI.LoginGUI.LoginFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AdminFrame extends JFrame {
+
+    private JPanel adminPanel;
+    private JButton manageOrganisationalUnitsButton;
+    private JButton manageUsersButton;
+
+    public AdminFrame() {
+
+        super("Admin Control Panel");
+
+        setDefaultLookAndFeelDecorated(true);
+        setLayout(new BorderLayout());
+
+        adminPanel = new JPanel();
+        manageOrganisationalUnitsButton = new JButton("Manage Organisational Units");
+        manageUsersButton = new JButton("Manage Users");
+        add(adminPanel, BorderLayout.CENTER);
+
+        manageOrganisationalUnitsButton.addActionListener(e -> manageOrg());
+        manageUsersButton.addActionListener(e -> manageUsers());
+        setupLayout();
+        setSize(500,500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationByPlatform(true);
+        setVisible(true);
+    }
+
+    private void setupLayout() {
+
+        adminPanel.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.PAGE_START;
+
+        constraints.gridy = 0;
+        constraints.gridx = 0;
+        adminPanel.add(manageOrganisationalUnitsButton, constraints);
+
+        constraints.gridy = 1;
+        constraints.gridx = 0;
+        adminPanel.add(manageUsersButton, constraints);
+
+    }
+
+    private void manageOrg() {
+
+    }
+
+    private void manageUsers() {
+
+        new ManageUsersFrame(new UsersData());
+
+    }
+
+}
